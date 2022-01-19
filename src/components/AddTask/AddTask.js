d6273663c821b3a78516b3d5269156efbd6b1a5e
Kengya <mkengya@gmail.com>
2022-01-18 22:46:51 -0500

    creating collection firestore reset imput

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,52 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button} from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import "./AddTask.scss";
 import { isEmpty } from "lodash";
-import { ReactComponent as Send } from "../../asset/send.svg"
+import { ReactComponent as Send } from "../../asset/send.svg";
 import "firebase/compat/app";
-import firebase from "../../utils/firebase";
-
-const db = firebase.firestore(firebase);
+import db from "../../utils/firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const AddTask = () => {
+  const [valueInput, setValueInput] = useState("");
 
-    const [valueInput, setValueInput] = useState("");
-
-
-    // useEffect(() => {
-    // const collectionTask = await addDoc(collection(db, "Task"){})
-    
-
-
-    // }, [])
-
-
-
-    const FormOnSubmit = (event) => {
-        event.preventDefault();
-        if(isEmpty(valueInput)){
-            
-        }else{
-            return false
-        }
-
-        console.log(valueInput);
-        
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    event.target.reset();
+    if (!isEmpty(valueInput)) {
+      const docRef = await addDoc(collection(db, "task"), {
+        name: valueInput,
+        completed: false,
+      });
+      return docRef;
+    } else {
+      return false;
     }
+  };
 
-
-    return(
-       <Form onSubmit={FormOnSubmit} className="add-task">
-
-           <input className="add-task__input"
-           type="text"
-           onChange={(event) => setValueInput(event.target.value)}
-           placeholder="Add task"
-           />
-           <Button type="submit"><Send/></Button>
-         
-       </Form>
-    )
-}
+  return (
+    <Form onSubmit={handleSubmit} className="add-task">
+      <input
+        className="add-task__input"
+        type="text"
+        onChange={(event) => setValueInput(event.target.value)}
+        placeholder="Add task"
+      />
+      <Button type="submit">
+        <Send />
+      </Button>
+    </Form>
+  );
+};
 
 export default AddTask;
