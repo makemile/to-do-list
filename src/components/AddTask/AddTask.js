e004470663dcc336de34c4c03da0ba33100d144c
Kengya <mkengya@gmail.com>
2022-01-18 20:44:17 -0500

    add state input

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button} from "react-bootstrap";
 import "./AddTask.scss";
+import { isEmpty } from "lodash";
 import { ReactComponent as Send } from "../../asset/send.svg"
+import "firebase/compat/app";
+import firebase from "../../utils/firebase";
 
+const db = firebase.firestore(firebase);
 
 const AddTask = () => {
 
-    const FormOnSubmit = () => {
-        console.log("formulario")
+    const [valueInput, setValueInput] = useState("");
+
+
+    // useEffect(() => {
+    // const collectionTask = await addDoc(collection(db, "Task"){})
+    
+
+
+    // }, [])
+
+
+
+    const FormOnSubmit = (event) => {
+        event.preventDefault();
+        if(isEmpty(valueInput)){
+            
+        }else{
+            return false
+        }
+
+        console.log(valueInput);
+        
     }
 
 
     return(
        <Form onSubmit={FormOnSubmit} className="add-task">
+
            <input className="add-task__input"
            type="text"
-           onChange={FormOnSubmit}
+           onChange={(event) => setValueInput(event.target.value)}
            placeholder="Add task"
            />
            <Button type="submit"><Send/></Button>
